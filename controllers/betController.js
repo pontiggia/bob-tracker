27f9dfab84c4da2a66bf050d3fd5e8144a2c8902
ponti <71143849+pontiggia@users.noreply.github.com>
2024-06-12 21:11:09 -0300

    Update betController.js

--- controllers/betController.js
+++ controllers/betController.js
@@ -5,22 +5,22 @@ const betFetch = require("../utils/fetch");
 const createBet = require("../utils/createBet");
 const updateStats = require("../utils/updateStats");
 
-cron.schedule("*/20 * * * * *", async () => {
-  // Fetch bets
-  const bets = await betFetch();
-  // Check if there are bets to insert
-  if (bets.length == 0) {
-    return console.log("No new bets");
-  } else {
-    // Insert bets into the database sequentially
-    for (const bet of bets) {
-      const newBet = await createBet(bet);
-      // Update user stats with the new bet
-      await updateStats(bet);
-      //console.log(newBet);
-    }
-  }
-});
+// cron.schedule("*/20 * * * * *", async () => {
+//   // Fetch bets
+//   const bets = await betFetch();
+//   // Check if there are bets to insert
+//   if (bets.length == 0) {
+//     return console.log("No new bets");
+//   } else {
+//     // Insert bets into the database sequentially
+//     for (const bet of bets) {
+//       const newBet = await createBet(bet);
+//       // Update user stats with the new bet
+//       await updateStats(bet);
+//       //console.log(newBet);
+//    }
+//  }
+//});
 
 exports.getAllBets = async (req, res) => {
   try {
